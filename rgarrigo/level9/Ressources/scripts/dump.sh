58401171073af6b8d6d3da0b3f8051d22189758d
rgarrigo <rgarrigo@student.42.fr>
2025-10-23 18:26:52 +0200

    Add level9

---
+++ rgarrigo/level9/Ressources/scripts/dump.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PWD=$(dirname $(readlink -f $0))
+FILES_DIR=${PWD}/../files
+DUMP_DIR=${PWD}/../dumps
+
+for file in $(find ${FILES_DIR} -type f -executable); do
+    objdump -d -C ${file} > ${DUMP_DIR}/$(basename ${file}).text
+    objdump -s ${file} > ${DUMP_DIR}/$(basename ${file}).dump
+done
+
+for file in $(find ${DUMP_DIR} -type f); do
+    chmod 440 ${file}
+done
