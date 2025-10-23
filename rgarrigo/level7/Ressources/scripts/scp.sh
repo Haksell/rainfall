394c40d7b4ff47466de9ce4d3b72473d36bc5de9
rgarrigo <rgarrigo@student.42.fr>
2025-10-23 14:39:21 +0200

    Add level7

---
+++ rgarrigo/level7/Ressources/scripts/scp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PORT=4244
+HOST=127.0.0.1
+
+PWD=$(dirname $(readlink -f $0))
+FILES_DIR=${PWD}/../files
+LEVEL_NAME=$(basename $(dirname $(dirname ${PWD})))
+
+export SSH_ASKPASS="${PWD}/askpass.sh"
+export SSH_ASKPASS_REQUIRE="force"
+
+scp -P ${PORT} "${LEVEL_NAME}@${HOST}:~/*" ${FILES_DIR}/
+scp -P ${PORT} "${LEVEL_NAME}@${HOST}:~/.*" ${FILES_DIR}/
+
+for file in $(find ${FILES_DIR} -type f); do
+    chmod -w ${file}
+done
