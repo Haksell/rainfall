a35fc48202a6cce47196845032948f27f042bdce
rgarrigo <rgarrigo@student.42.fr>
2025-10-24 18:13:46 +0200

    Better run.sh script

--- rgarrigo/server/run.sh
+++ rgarrigo/server/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-HOST_PORT=4244
+PWD=$(dirname $(readlink -f $0))
 
+HOST_PORT=4244
 GUEST_IP=10.0.2.15
 GUEST_PORT=4242
 
-qemu-system-x86_64																														\
-	-cdrom RainFall.iso																													\
+qemu-system-x86_64															\
+	-cdrom ${PWD}/RainFall.iso												\
 	-nic user,hostfwd=tcp::${HOST_PORT}-${GUEST_IP}:${GUEST_PORT}
