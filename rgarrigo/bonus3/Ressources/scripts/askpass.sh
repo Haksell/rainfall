2ce57af271e1bf4cb94fab4246b2392170a87014
rgarrigo <rgarrigo@student.42.fr>
2025-10-24 18:09:42 +0200

    Add bonus3

---
+++ rgarrigo/bonus3/Ressources/scripts/askpass.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+PWD=$(dirname $(readlink -f $0))
+LEVEL_NAME=$(basename $(dirname $(dirname ${PWD})))
+
+if [ ${LEVEL_NAME##level} != ${LEVEL_NAME} ]; then
+    LEVEL_ID=${LEVEL_NAME##level}
+    PREVIOUS_LEVEL_ID=$(echo "${LEVEL_ID} - 1" | bc)
+    PREVIOUS_LEVEL_NAME=level${PREVIOUS_LEVEL_ID}
+else
+    LEVEL_ID=${LEVEL_NAME##bonus}
+    if [ ${LEVEL_ID} -eq 0 ]; then
+        PREVIOUS_LEVEL_NAME=level9
+    else
+        PREVIOUS_LEVEL_ID=$(echo "${LEVEL_ID} - 1" | bc)
+        PREVIOUS_LEVEL_NAME=bonus${PREVIOUS_LEVEL_ID}
+    fi
+fi
+FLAG_FILE=${PWD}/../../../${PREVIOUS_LEVEL_NAME}/flag
+
+cat ${FLAG_FILE}
