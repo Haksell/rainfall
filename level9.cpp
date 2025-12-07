8f47e424179e69fe5da51b7c08611b17b1684a5b
Haksell <axel.brisse@gmail.com>
2025-12-07 10:11:54 +0100

    level9 decompiled but im lost

---
+++ level9.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <cstring>
+
+class N {
+  public:
+    N(int value) : _value(value) {}
+
+    void setAnnotation(const char* text) {
+        std::memcpy(_annotation, text, std::strlen(text));
+    }
+
+    int operator+(const N& other) { return this->_value + other._value; }
+    int operator-(const N& other) { return this->_value - other._value; }
+
+  private:
+    char _annotation[100];
+    int _value;
+};
+
+int main(int argc, char** argv) {
+    if (argc < 2) std::exit(1);
+
+    N* a = new N(5);
+    N* b = new N(6);
+
+    a->setAnnotation(argv[1]);
+
+    b->operator+(*a);
+
+    return 0;
+}
