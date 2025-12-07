64ffafa78fa15a40dfb10f54ae0dea42673a91a7
Haksell <axel.brisse@gmail.com>
2025-12-07 09:16:24 +0100

    level7 done (dont use wildcard you fucking donkey)

--- level7.c
+++ level7.c
@@ -3,34 +3,33 @@
 #include <string.h>
 #include <time.h>
 
-void m(void* param_1, int param_2, char* param_3, int param_4, int param_5)
+char c[68];
 
-{
-    time_t tVar1;
+typedef struct item_t {
+    int id;
+    char* buf;
+} item_t;
 
-    tVar1 = time((time_t*)0x0);
-    printf("%s - %d\n", c, tVar1);
+void m() {
+    printf("%s - %d\n", c, (int)time(NULL));
     return;
 }
 
-int main(int param_1, char** param_2) {
-    char* puVar1;
-    void* pvVar2;
-    char* puVar3;
-    FILE* __stream;
+int main(int argc, char** argv) {
+    item_t* item1 = malloc(sizeof(item_t));
+    item1->id = 1;
+    item1->buf = malloc(8);
 
-    puVar1 = malloc(8);
-    *puVar1 = 1;
-    pvVar2 = malloc(8);
-    puVar1[1] = pvVar2;
-    puVar3 = malloc(8);
-    *puVar3 = 2;
-    pvVar2 = malloc(8);
-    puVar3[1] = pvVar2;
-    strcpy((char*)puVar1[1], *(char**)(param_2 + 4));
-    strcpy((char*)puVar3[1], *(char**)(param_2 + 8));
-    __stream = fopen("/home/user/level8/.pass", "r");
-    fgets(c, 0x44, __stream);
+    item_t* item2 = malloc(sizeof(item_t));
+    item2->id = 2;
+    item2->buf = malloc(8);
+
+    strcpy(item1->buf, argv[1]);
+    strcpy(item2->buf, argv[2]);
+
+    FILE* stream = fopen("/home/user/level8/.pass", "r");
+    fgets(c, 68, stream);
+
     puts("~~");
     return 0;
 }
