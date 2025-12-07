03a44b8d63fc5ad9a2934ab80563692b2245b47f
Haksell <axel.brisse@gmail.com>
2025-12-07 05:32:45 +0100

    decompile level5

---
+++ level5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void o(void) {
+    system("/bin/sh");
+    _exit(1);
+}
+
+void n(void) {
+    char s[520];
+
+    fgets(s, 512, stdin);
+    printf(s);
+    exit(1);
+}
+
+int main(void) { n(); }
