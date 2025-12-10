72869201f5678578a0855ec88e862ca2a708a47f
Haksell <axel.brisse@gmail.com>
2025-12-11 00:03:20 +0100

    .

---
+++ level4/level4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int m;
+
+void p(char* format) { printf(format); }
+
+void n() {
+    char s[520];
+    fgets(s, 512, stdin);
+    p(s);
+    if (m == 16930116) system("/bin/cat /home/user/level5/.pass");
+}
+
+int main() { n(); }
