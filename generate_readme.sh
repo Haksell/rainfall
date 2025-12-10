3a5478a1a55d73ded308ebc5dfe53f9d92368d33
Haksell <axel.brisse@gmail.com>
2025-12-10 21:35:15 +0100

    .

--- generate_readme.sh
+++ generate_readme.sh
@@ -4,7 +4,8 @@ FILE=README.md
 
 echo "# Rainfall" > $FILE
 echo >> $FILE
-for level in level{0..9} bonus{0..3}; do
+# for level in level{0..9} bonus{0..3}; do
+for level in level{0..1}; do
     # Adjust the image paths to be relative to the root directory
     sed "s|!\[Alt text\](|&$level/resources/|g" $level/README.md >> $FILE
     echo -e -n "\n\n" >> $FILE
